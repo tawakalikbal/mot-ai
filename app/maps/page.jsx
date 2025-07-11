b370cf8feb7fa0606dde2b005be48b674cbb50e7
amgeraldo14 <73383622+amgeraldo14@users.noreply.github.com>
2025-07-11 17:03:11 +0700

    map function done, on progress search function

--- app/maps/page.jsx
+++ app/maps/page.jsx
@@ -5,7 +5,9 @@ import { useState } from 'react'
 import { ChatbotWidget } from '@/components/ChatbotWidget'
 import { GoogleMap } from '@/components/GoogleMap'
 import locations from '@/lib/location.json'
+import { ArrowLeft, MapPin, Search, StarsIcon } from 'lucide-react'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { useRef } from 'react'
 
 // const MapComponent = dynamic(() => import("@/components/LeafletMap"), {
 //   ssr: false,
@@ -20,6 +22,7 @@ export default function Page() {
   const router = useRouter()
   const searchParams = useSearchParams()
   const [selectedPlace, setSelectedPlace] = useState({})
+  const chatbotRef = useRef(null)
   // const touristSpots = useMemo(() => locations.tourist_spots, []);
   // const MemoizedGoogleMap = React.memo(GoogleMap);
   const locationId = searchParams.get('locationId')
@@ -27,13 +30,19 @@ export default function Page() {
     setSelectedPlace(markerData)
     const params = new URLSearchParams(searchParams)
     params.set('locationId', markerData.id)
-
     router.push(`?${params.toString()}`)
   }
   return (
     <div className='h-screen'>
-      <ChatbotWidget />
+      <ChatbotWidget ref={chatbotRef} />
       {/* <MapComponent /> */}
+      {selectedPlace?.id && (
+        <MapContent
+          selectedPlace={selectedPlace}
+          chatbotRef={chatbotRef}
+          setSelectedPlace={setSelectedPlace}
+        />
+      )}
       {!locationId && <MapSearch />}
       <GoogleMap
         key='main-map'
@@ -42,6 +51,67 @@ export default function Page() {
         className='w-full h-full rounded-lg shadow-lg'
       />
     </div>
+  )
+}
+
+export const MapContent = ({ selectedPlace, chatbotRef, onEnter, setSelectedPlace }) => {
+  const [value, setValue] = useState('')
+  return (
+    <aside className='bg-white fixed left-10 top-1/2 -translate-y-1/2 z-100 h-auto max-h-[80vh] w-full max-w-[20vw] rounded-xl p-4  flex flex-col gap-4'>
+      <section className='relative'>
+        <Search className='absolute left-2 top-1/2 -translate-y-1/2' size={16} color='gray' />
+        <input
+          className='focus:outline-0 border w-full pl-8 pr-4 rounded-md p-1.5'
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === 'Enter') {
+              const newLocation = locations.tourist_spots.find(
+                (spot) => spot.name.toLowerCase() === value.toLowerCase()
+              )
+              setSelectedPlace(newLocation)
+            }
+          }}
+        />
+      </section>
+      <section className='flex gap-1 items-center cursor-pointer'>
+        <ArrowLeft size={20} />
+        <p>Back</p>
+      </section>
+      <section className='overflow-auto scrollbar-hide'>
+        <img src={selectedPlace.image} className='aspect-video rounded-xl object-cover mb-5' />
+        <p className='text-blue-600 font-semibold text-sm '>{selectedPlace.type}</p>
+        <h2 className='font-bold text-xl mb-1'>{selectedPlace.name}</h2>
+        <div className='flex text-slate-600 items-center gap-1 mb-2'>
+          <MapPin size={16} />
+          <p className='text-sm'>{selectedPlace.location}</p>
+        </div>
+        <p className='whitespace-pre-wrap '>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium commodi libero maxime
+          laborum deleniti ratione illum obcaecati odio, at incidunt eligendi cum repellat tenetur
+          quae est a voluptatum ipsum hic architecto accusantium nam. Voluptates tempora suscipit
+          vel consequatur esse officia facilis ad natus laboriosam excepturi nesciunt dolore
+          explicabo nam, distinctio doloremque recusandae ullam nihil, libero error eligendi maxime
+          mollitia! Hic nulla laborum quidem, eligendi voluptates mollitia cumque amet, in fugit
+          accusantium aliquam beatae ad animi numquam dicta officia delectus nesciunt assumenda, id
+          corporis? Voluptatem nam excepturi blanditiis commodi, molestias unde recusandae soluta
+          repellendus. Asperiores animi vero quis consequuntur corrupti! Dolorem.
+        </p>
+      </section>
+      <section>
+        <button
+          className='bg-black text-white w-full rounded-full px-2 py-3 cursor-pointer flex items-center justify-center font-semibold gap-2'
+          onClick={() =>
+            chatbotRef.current.sendChat(
+              `Hi AiYu! Can you tell me more about ${selectedPlace.name}?`
+            )
+          }
+        >
+          Ask AiYu
+          <StarsIcon size={20} />
+        </button>
+      </section>
+    </aside>
   )
 }
 
