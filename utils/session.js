f4eab279d1035d77f0631c83d1c259a35f623750
tawakal <mohamad.ikbal@magnusdigital.co.id>
2025-07-14 16:59:09 +0700

    fix session generator

--- utils/session.js
+++ utils/session.js
@@ -1,16 +1,40 @@
+// export function getOrCreateSessionId() {
+//   let sessionId = sessionStorage.getItem('sessionId');
+//   if (!sessionId) {
+//     sessionId = crypto.randomUUID();
+//     sessionStorage.setItem('sessionId', sessionId);
+//   }
+//   return sessionId;
+// }
+
+// export function getOrCreateUserId() {
+//   let userId = sessionStorage.getItem('userId');
+//   if (!userId) {
+//     userId = crypto.randomUUID();
+//     sessionStorage.setItem('userId', userId);
+//   }
+//   return userId;
+// }
+
+import { v4 as uuidv4 } from 'uuid';
+
 export function getOrCreateSessionId() {
+  if (typeof window === 'undefined') return ''; // Prevent errors during SSR
+
   let sessionId = sessionStorage.getItem('sessionId');
   if (!sessionId) {
-    sessionId = crypto.randomUUID();
+    sessionId = uuidv4();
     sessionStorage.setItem('sessionId', sessionId);
   }
   return sessionId;
 }
 
 export function getOrCreateUserId() {
+  if (typeof window === 'undefined') return ''; // Prevent errors during SSR
+
   let userId = sessionStorage.getItem('userId');
   if (!userId) {
-    userId = crypto.randomUUID();
+    userId = uuidv4();
     sessionStorage.setItem('userId', userId);
   }
   return userId;
